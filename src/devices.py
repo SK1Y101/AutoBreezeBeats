9fa8c6679f826eb5e8db10c8961e88e35a885a5d
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2024-06-24 10:55:14 +0100

    add periodic scanning to backend

--- src/devices.py
+++ src/devices.py
@@ -1,19 +1,44 @@
 from __future__ import annotations
 
+import asyncio
 from dataclasses import dataclass
 from subprocess import CalledProcessError
-from typing import Optional
+from typing import Any
 
 from bleak import BleakClient, BleakScanner
+from fastapi import HTTPException
+from pydantic import BaseModel
 
-from common import load_data, run, save_data
+from .common import load_data, run, save_data
+
+
+class ConnectError(HTTPException):
+    def __init__(self, address: str) -> None:
+        super().__init__(status_code=500, detail=f"Failed to connect to {address}")
+
+
+class DisconnectError(HTTPException):
+    def __init__(self, address: str) -> None:
+        super().__init__(status_code=500, detail=f"Failed to disconnect from {address}")
+
+
+class DeviceAction(BaseModel):
+    address: str
 
 
 @dataclass
 class Device:
     address: str
-    name: Optional[str] = "Unknown device"
+    name: str = "Unknown device"
     connected: bool = False
+
+    @property
+    def to_dict(self) -> dict[str, str | bool]:
+        return {
+            "name": self.name,
+            "address": self.address.lower(),
+            "connected": self.connected,
+        }
 
     @property
     def sink_address(self) -> str:
@@ -39,9 +64,16 @@ class DeviceManager:
         self.filename = "connected_devices.json"
         self.load_devices()
 
-    async def scan_devices(self) -> list[Device]:
+    async def scan_devices(self) -> None:
         devices = await BleakScanner.discover()
-        return [Device(name=device.name, address=device.address) for device in devices]
+        self.devices = [
+            Device(name=device.name or "Unknown device", address=device.address)
+            for device in devices
+        ]
+
+    @property
+    def list_devices(self) -> list[dict[str, Any]]:
+        return [device.to_dict for device in self.devices]
 
     def _find_device_(self, address: str) -> Device:
         device = next(
@@ -52,7 +84,7 @@ class DeviceManager:
             self.devices.append(device)
         return device
 
-    async def connect_deice(self, address: str) -> None:
+    async def connect_device(self, address: str) -> bool:
         device = self._find_device_(address=address)
 
         await device.connect()
@@ -60,14 +92,18 @@ class DeviceManager:
 
         self.set_audio_sinks()
         self.save_devices()
+        return device.connected is True
 
-    async def disconnect_device(self, address: str) -> None:
+    async def disconnect_device(self, address: str) -> bool:
         device = self.clients.pop(address, None)
         if device:
             await device.client.disconnect()
+        else:
+            return False
 
         self.set_audio_sinks()
         self.save_devices()
+        return device.connected is False
 
     def set_audio_sinks(self) -> None:
         for device in self.clients.values():
@@ -79,6 +115,18 @@ class DeviceManager:
             except CalledProcessError as e:
                 print(f"Failed to set audio sink to {device.address}: {e}")
 
+    async def sync_devices(self) -> None:
+        """Ensure the connected devices match what we think is connected."""
+        for device in self.devices:
+            if device.address in self.clients.keys():
+                device.connected = True
+            if device.connected:
+                await device.connect()
+                self.clients[device.address] = device
+            else:
+                await device.disconnect()
+                self.clients.pop(device.address, None)
+
     def save_devices(self) -> None:
         device_dict = {device.name: device.address for device in self.devices}
         save_data(self.filename, device_dict)
@@ -89,5 +137,19 @@ class DeviceManager:
             Device(name=name, address=address) for name, address in device_dict.items()
         ]
 
+    async def periodic_scan(self, interval: int = 30) -> None:
+        while True:
+            await self.scan_devices()
+            await self.sync_devices()
+            await asyncio.sleep(interval)
+
 
 device_manager = DeviceManager()
+
+
+async def start_periodic_scan() -> None:
+    await device_manager.periodic_scan()
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(start_periodic_scan())
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from .devices import ConnectError, DeviceAction, DisconnectError, device_manager
 from .host_device import get_device_details
 
 app = FastAPI()
@@ -23,6 +24,29 @@ async def index(request: Request) -> HTMLResponse:
             "host": get_device_details(),
         },
     )
+
+
+@app.get("/devices")
+async def list_devices() -> list[dict[str, str | bool]]:
+    return device_manager.list_devices
+
+
+@app.post("/devices/connect")
+async def bluetooth_connect(action: DeviceAction):
+    address = action.address
+    print(f"request to connect {address} recieved")
+    if not await device_manager.connect_device(address):
+        raise ConnectError(address)
+    return {"status": "Connected"}
+
+
+@app.post("/devices/disconnect")
+async def bluetooth_disconnect(action: DeviceAction):
+    address = action.address
+    print(f"request to connect {address} recieved")
+    if not await device_manager.disconnect_device(address):
+        raise DisconnectError(address)
+    return {"status": "Disconnected"}
 
 
 if __name__ == "__main__":
