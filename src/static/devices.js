eecf0fdced75a5f43874e93073109bf1ccc43969
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2024-06-24 18:16:58 +0100

    ui changes

--- src/static/devices.js
+++ src/static/devices.js
@@ -1,26 +1,49 @@
+async function setSink() {
+    const addr = this.dataset??false ? this.dataset.address : null;
+    fetch("/devices/set-sink", {
+        method:"PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({address: addr}),
+    })
+    .then(response => {
+        console.log(response);
+        if (response.ok) {
+            location.reload();
+        } else {
+            console.error("Could not set sink")
+        }
+    })
+    .catch(error => console.error("Could not set sink", error));
+}
+
 async function toggleDevice() {
     const addr = this.dataset.address;
-    const action = this.dataset.connected ? "disconnect" : "connect";
+    const action = this.classList.contains("disconnected") ? "connect" : "disconnect";
 
-    const response = await fetch(`/devices/${action}`, {
+    await fetch(`/devices/${action}`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({address: addr}),
-    });
-
-    if (response.ok) {
-        if (this.dataset.connected) {
-            this.classList.remove("connected");
-            this.classList.add("disconnected");
+    })
+    .then(response => {
+        console.log(response);
+        if (response.ok) {
+            if (action === "connect") {
+                this.classList.remove("connected");
+                this.classList.add("disconnected");
+            } else {
+                this.classList.remove("disconnected");
+                this.classList.add("connected");
+            };
         } else {
-            this.classList.remove("disconnected");
-            this.classList.add("connected");
+            console.error(`Failed to toggle connection for ${addr}`);
         };
-    } else {
-        console.error(`Failed to toggle connection for ${addr}`);
-    };
+    })
+    .catch(error => console.error('Connection failed:', error))
 }
 
 async function fetchDevices() {
@@ -31,10 +54,12 @@ async function fetchDevices() {
 
     devices.forEach(device => {
         const deviceItem = document.createElement("button");
+        const devicePlay = document.createElement("button");
         deviceItem.dataset.address = device.address;
-        deviceItem.dataset.connected = device.connected;
+        devicePlay.dataset.address = device.address;
 
         deviceItem.textContent = device.name + " - " + device.address;
+        devicePlay.textContent = "Set as playback device"
 
         if (device.connected) {
             deviceItem.classList.add("connected");
@@ -43,14 +68,23 @@ async function fetchDevices() {
             deviceItem.classList.add("disconnected");
         }
 
+        if (device.primary) {
+            devicePlay.classList.add("connected");
+        }
+        else {
+            devicePlay.classList.add("disconnected");
+        }
+
         deviceItem.addEventListener("click", toggleDevice);
+        devicePlay.addEventListener("click", setSink);
 
         deviceTable.appendChild(deviceItem);
+        deviceTable.appendChild(devicePlay);
     });
 
     deviceList.replaceChildren(deviceTable);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    setInterval(fetchDevices, 10000);
+    setInterval(fetchDevices, 1000);
 });
